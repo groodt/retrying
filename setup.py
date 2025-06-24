98b6a0121ecfca19033b197d1d8c837c5f290068
Greg Roodt <groodt@gmail.com>
2025-06-24 19:57:10 +1000

    Release v1.3.6

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open("HISTORY.rst") as file_history:
 
 setup(
     name="retrying",
-    version="1.3.6-dev",
+    version="1.3.6",
     description="Retrying",
     long_description=readme + "\n\n" + history,
     author="Greg Roodt",
